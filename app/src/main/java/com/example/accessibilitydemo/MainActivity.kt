1fc3439caa894f9f06a7de32f9309cf107e48c53
Rui Ferreira Oliveira <E1238@itsector.pt>
2021-04-08 16:49:29 +0100

    Main activity form

--- app/src/main/java/com/example/accessibilitydemo/MainActivity.kt
+++ app/src/main/java/com/example/accessibilitydemo/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.accessibilitydemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.accessibilitydemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.nextBtn.setOnClickListener {
+            val intent = Intent(this, ConfirmActivity::class.java);
+            startActivity(intent);
+        }
     }
 }
