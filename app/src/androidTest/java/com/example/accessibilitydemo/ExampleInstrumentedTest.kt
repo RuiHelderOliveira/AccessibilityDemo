ac3d69d1216dd31f379044936c9610f5243dd4ba
Itsector <itsector@Suportes-MacBook-Pro.local>
2021-04-21 12:13:37 +0100

    Implementation of small ui tests and activation of accessibility tests. Minor fix.

--- app/src/androidTest/java/com/example/accessibilitydemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/accessibilitydemo/ExampleInstrumentedTest.kt
@@ -7,6 +7,18 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.accessibility.AccessibilityChecks
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
+import com.example.accessibilitydemo.activities.MainActivity
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +26,35 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+    @Before
+    fun setUp() {
+        AccessibilityChecks.enable()
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.accessibilitydemo", appContext.packageName)
+    }
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testTransferClick() {
+        onView(withId(R.id.transfer_button))
+            .perform(click())
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testShareClick() {
+        onView(withId(R.id.share_button))
+            .perform(click())
+            .check(matches(isDisplayed()))
     }
 }
