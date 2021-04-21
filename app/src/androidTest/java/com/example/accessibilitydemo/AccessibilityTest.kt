cf8f329f0b47853a6b4a28a64b222bf5d75e8c10
Itsector <itsector@Suportes-MBP.lan>
2021-04-21 18:04:49 +0100

    Add small UI tests.

--- app/src/androidTest/java/com/example/accessibilitydemo/AccessibilityTest.kt
+++ app/src/androidTest/java/com/example/accessibilitydemo/AccessibilityTest.kt
@@ -1,33 +1,33 @@
 package com.example.accessibilitydemo
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.graphics.Color
+import android.util.Log
+import androidx.core.graphics.ColorUtils
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.accessibility.AccessibilityChecks
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.accessibilitydemo.activities.MainActivity
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-import org.junit.Before
-
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.accessibility.AccessibilityChecks
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.filters.LargeTest
-import com.example.accessibilitydemo.activities.MainActivity
-import org.junit.Rule
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class ExampleInstrumentedTest {
+class AccessibilityTest {
 
     @Before
     fun setUp() {
@@ -47,14 +47,26 @@ class ExampleInstrumentedTest {
     @Test
     fun testTransferClick() {
         onView(withId(R.id.transfer_button))
-            .perform(click())
             .check(matches(isDisplayed()))
+            .perform(click())
     }
 
     @Test
     fun testShareClick() {
         onView(withId(R.id.share_button))
-            .perform(click())
             .check(matches(isDisplayed()))
+            .perform(click())
+    }
+
+    @Test
+    fun testContrast() {
+        val foregroundColor = Color.WHITE
+        val backgroundColor = Color.BLUE
+
+        val contrast = ColorUtils.calculateContrast(foregroundColor, backgroundColor)
+
+        Log.d("Test", contrast.toString())
+
+        assertTrue(contrast > 3);
     }
 }
